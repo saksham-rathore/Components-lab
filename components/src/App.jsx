c8c0963366d87d0168fd0ee9840fd0d4c6830e47
saksham762007 <samrathore07062007@gmail.com>
2026-07-03 02:27:34 +0530

    UI/UX

---
+++ components/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Mid_node from './assets/Components/Mid_node'
+import Left_nodes from './assets/Components/Left_nodes'
+import Right_nodes from './assets/Components/Right_nodes'
+import Animate_lines from './assets/Components/Animate_lines'
+
+const App = () => {
+  return (
+    <div>
+      <Mid_node />
+      <Left_nodes />
+      <Right_nodes />
+      <Animate_lines />
+    </div>
+  )
+}
+
+export default App
