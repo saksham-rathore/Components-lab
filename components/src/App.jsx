38b5d14591b558040e2a47136dd8a70fb226813c
saksham762007 <samrathore07062007@gmail.com>
2026-07-03 15:34:23 +0530

    feat: initialize App component structure and configure Tailwind CSS integration

--- components/src/App.jsx
+++ components/src/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import Mid_node from './assets/Components/Mid_node'
-import Left_nodes from './assets/Components/Left_nodes'
 import Leftnode from './assets/Components/Leftnode'
 import Right_nodes from './assets/Components/Right_nodes'
 import Oneline from './assets/Components/Oneline'
 import Animate_lines from './assets/Components/Animate_lines'
+// import './index.css'
+
 
 const App = () => {
   return (
-    <div>
-      <Left_nodes />
-      <Animate_lines />
+    <div className='flex justify-center w-full h-screen'>
       <Leftnode />
+      <Animate_lines />
       <Oneline />
-      <Mid_node />
       <Right_nodes />
     </div>
   )
