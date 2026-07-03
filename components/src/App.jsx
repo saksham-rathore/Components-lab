96c07e44cd63a96c7e1f60560a48c30ce2e528f4
saksham762007 <samrathore07062007@gmail.com>
2026-07-03 18:11:03 +0530

    feat: implement animated SVG path components and base styling for the dashboard layout

--- components/src/App.jsx
+++ components/src/App.jsx
@@ -3,16 +3,29 @@ import Leftnode from './assets/Components/Leftnode'
 import Right_nodes from './assets/Components/Right_nodes'
 import Oneline from './assets/Components/Oneline'
 import Animate_lines from './assets/Components/Animate_lines'
+import Usernode from './assets/Components/Usernode'
 // import './index.css'
-
 
 const App = () => {
   return (
-    <div className='flex justify-center w-full h-screen'>
-      <Leftnode />
-      <Animate_lines />
-      <Oneline />
-      <Right_nodes />
+    <div className="flex items-center justify-center w-full min-h-screen overflow-hidden bg-white">
+      <div className="flex items-center" style={{ transform: 'scale(0.35)', transformOrigin: 'center' }}>
+        <div className="z-10 relative">
+          <Leftnode />
+        </div>
+        <div className="z-0 -ml-[112px] relative">
+          <Animate_lines />
+        </div>
+        <div className="z-10 -ml-[162px] relative">
+          <Right_nodes />
+        </div>
+        <div className="z-0 -ml-[162px] relative">
+          <Oneline />
+        </div>
+        <div className="z-10 -ml-[112px] relative">
+          <Usernode />
+        </div>
+      </div>
     </div>
   )
 }
--- components/src/assets/Components/Oneline.jsx
+++ components/src/assets/Components/Oneline.jsx
@@ -4,7 +4,7 @@ const Oneline = () => {
     return (
         <div>
             <svg width="1033" height="9" viewBox="0 0 1033 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <line x1="0.00804069" y1="6.16554" x2="1032.01" y2="2.00006" stroke="#D8D8D8" stroke-width="4" />
+                <line x1="0.00804069" y1="6.16554" x2="1032.01" y2="2.00006" stroke="#D8D8D8" strokeWidth="4" className="animate-draw" style={{ animationDelay: '1.5s' }} />
             </svg>
         </div>
     )
